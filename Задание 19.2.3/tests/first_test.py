c5e7464572f99258a9cec4a2e343dad5012ad0d4
elfbar59 <105165128+elfbar59@users.noreply.github.com>
2022-12-12 07:31:21 +0300

    Add files via upload

---
+++ Задание 19.2.3/tests/first_test.py
@@ -0,0 +1,11 @@
+import pytest
+from app.calculator import Calculator
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
---
+++ Задание 19.2.3/tests/test3.py
@@ -0,0 +1,12 @@
+import pytest
+from app.calculator import Calculator
+
+
+class TestCalc:
+    def setup(self):
+
+
+        self.calc = Calculator
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.subtraction(self, 2, 2) == 0
